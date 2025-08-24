2a1f402e3ab4ff80373a10ffa91c3aa7cc10e14c
joulesev <stunt.man.joules@gmail.com>
2025-08-23 19:08:19 -0600

    app.py

--- app.py
+++ app.py
@@ -82,8 +82,8 @@ with col1:
                     try:
                         model = genai.GenerativeModel('gemini-1.5-flash-latest')
                         prompt = f"""
-                        Analiza el siguiente texto y reestructúralo en formato Markdown. 
-                        Crea un título, un resumen y secciones lógicas. Resalta los datos clave en negrita.
+                        Analiza el siguiente texto y reestructúralo completo, sin omitir informacion en formato Markdown. 
+                        Despues, en una seccion nueva, crea un título, un resumen y secciones lógicas. Resalta los datos clave en negrita.
 
                         --- TEXTO ORIGINAL ---
                         {st.session_state.original_content}
